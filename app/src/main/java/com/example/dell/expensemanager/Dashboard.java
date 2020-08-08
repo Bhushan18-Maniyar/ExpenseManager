b1cd6fe4c564828a715379e600fc08d52a748957
Bhushan <bhushanmaniyar@gmail.com>
2020-08-08 17:30:07 +0530

    Navigation drawer Bug fixed

--- app/src/main/java/com/example/dell/expensemanager/Dashboard.java
+++ app/src/main/java/com/example/dell/expensemanager/Dashboard.java
@@ -4,20 +4,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.client.AuthData;
-import com.firebase.client.Firebase;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 public class Dashboard extends AppCompatActivity {
@@ -31,7 +24,7 @@ public class Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.navigation_drawer);
+        setContentView(R.layout.activity_dashboard);
 
 
         toolbar = findViewById(R.id.toolbar);
@@ -44,7 +37,8 @@ public class Dashboard extends AppCompatActivity {
         navigationView = findViewById(R.id.navigation_view);
 
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(Dashboard.this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
-        drawerLayout.setDrawerListener(drawerToggle);
+        drawerLayout.addDrawerListener(drawerToggle);
+//        drawerLayout.setDrawerListener(drawerToggle);
         drawerToggle.syncState();
 
         email = findViewById(R.id.email);
@@ -59,9 +53,19 @@ public class Dashboard extends AppCompatActivity {
             // The user's ID, unique to the Firebase project. Do NOT use this value to
             // authenticate with your backend server, if you have one. Use
             // FirebaseUser.getIdToken() instead.
-            email.setText(user.getUid().toString());
-            Toast.makeText(this,user.getUid(),Toast.LENGTH_LONG).show();
+            email.setText(user.getUid());
+            Toast.makeText(this, user.getUid(), Toast.LENGTH_LONG).show();
 
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
